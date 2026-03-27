2bbeb3bd933a87f58ad4b9d31d20c79548e6c81c
bostler3 <bostler3@gmail.com>
2026-03-27 13:35:27 -0600

    Additional initial setup

---
+++ routes/actors.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const actorsController = require("../controllers/actors");
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use("/", require("./routes"));
+
 app.listen(port, () => {
   console.log(`Node is running on port ${port}`);
 });
