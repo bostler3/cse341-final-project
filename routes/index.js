8506a7aeb3612beee259aeb8c855a4cb2c9fa061
Francesco Foresta <f.foresta86@pm.me>
2026-04-04 08:39:17 +0200

    Implement root endpoint for API status
    Add a root endpoint that returns API status and available endpoints.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+	res.status(200).json({
+		message: "Movie API is running",
+		endpoints: ["/movies", "/actors", "/api-docs"]
+	});
+});
+
 router.use('/movies', require('./movies'));
 
 router.use("/actors", require("./actors"));
